645b410369ea0e0a7c1e6d76b24d0f7490ea97dd
Admin <funedy@yandex.ru>
2023-12-24 15:12:17 +0300

    fix task 3

--- src/task3/Main.java
+++ src/task3/Main.java
@@ -7,11 +7,20 @@ import java.util.List;
 public class Main {
     public static ArrayList<Student> integers = new ArrayList<Student>();
     public static void main(String[] args) {
-        StudentTreatment.create();
-        StudentTreatment.Graded();
+        create();
+        StudentTreatment.Graded(integers);
         StudentTreatment.printStudents(integers,4);
 
     }
 
+    public static void create(){
+        Main.integers.add(new Student("Oleg", 2,3,5));
+        Main.integers.add(new Student("Petr", 4,2,1));
+        Main.integers.add(new Student("Kira", 1,3,4));
+        Main.integers.add(new Student("Pupa", 1,3,2));
+        Main.integers.add(new Student("Egor", 1,1,2));
+        Main.integers.forEach(System.out::println);
+        System.out.println("===========================");
+    }
 
 }
